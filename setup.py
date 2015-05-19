3dece65aff492ff8af7b829aeba8a57b46781276
James Johnson <james@narly.me>
2015-05-19 07:26:56 -0500

    getting ready for auto-testing

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os, sys
+from setuptools import setup
+
+setup(
+    # metadata
+    name='pfp',
+    description='An 010 template interpreter for Python',
+    long_description="""
+		pfp is an 010 template interpreter for Python. It accepts an
+		input data stream and an 010 template and returns a modifiable
+		DOM of the parsed data. Extensions have also been added to the
+		010 template syntax to allow for linked fields (e.g. checksums,
+		length calculations, etc), sub structures in compressed data,
+		etc.
+    """,
+    license='MIT',
+    version='0.1',
+    author='James Johnson',
+    maintainer='James Johnson',
+    author_email='d0c.s4vage@gmail.com',
+    url='https://github.com/d0c-s4vage/pfp',
+    platforms='Cross Platform',
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',],
+    packages=['pfp'],
+)
