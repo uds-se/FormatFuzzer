b4be74db66b0780fc153ae915795412c4eebd52b
James Johnson <d0c.s4vage@gmail.com>
2015-08-13 07:13:37 -0500

    version bump for cpp_args default change

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ setup(
 		etc.
     """,
     license='MIT',
-    version='0.1.8',
+    version='0.1.9',
     author='James Johnson',
     maintainer='James Johnson',
     author_email='d0c.s4vage@gmail.com',
     url='https://github.com/d0c-s4vage/pfp',
     platforms='Cross Platform',
-	download_url="https://github.com/d0c-s4vage/pfp/tarball/v0.1.8",
+	download_url="https://github.com/d0c-s4vage/pfp/tarball/v0.1.9",
 	install_requires = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).read().split("\n"),
     classifiers = [
         'Programming Language :: Python :: 2',
