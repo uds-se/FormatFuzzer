67910ffd72a1b5f54f2987a6fa0da3aada04ac10
Tim Strazzere <Tim.Strazzere@mylookout.com>
2015-08-19 22:51:57 -0700

    Add Adler32 functionality to Checksum.

---
+++ tests/test_compat_checksums.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+import os
+import sys
+import utils
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+class TestCompat(unittest.TestCase, utils.UtilsMixin):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_adler32(self):
+		dom = self._test_parse_build(
+			"test\x00",
+			"""
+				string test;
+				Printf("%X", Checksum(CHECKSUM_ADLER32, 0, 1, -1, -1));
+			""",
+			stdout="750075",
+			# Required for CHECKSUM_ADLER32 to be found
+			predefines=True
+		)
