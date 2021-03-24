e7b5ffb7c0e2176f7d55b00a593c87027e567ae6
Pit Jost <s8pijost@stud.uni-saarland.de>
2021-03-24 14:45:11 +0100

    Remove generation files

--- checkers/avi.sh
+++ checkers/avi.sh
@@ -1,1 +1,4 @@
-ffmpeg -y -f avi -i - -c:v mpeg4 -c:a copy output.avi <out.avi 2>/dev/null
+FFMPEG="/usr/local/bin/ffmpeg"
+FFMPEG="/Users/admin/FFmpeg/ffmpeg"
+
+$FFMPEG -y -f avi -i - -c:v mpeg4 -c:a copy output.mp4 <out.avi 2>/dev/null
