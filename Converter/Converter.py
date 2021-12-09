06a130440718e32a246b2d548d0437c8de3836ec
match246 <s8daerce@stud.uni-saarland.de>
2021-12-09 17:06:21 +0100

    Extended class stucture

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -6,9 +6,12 @@ datatypes = {"u4": "uint32", "u1": "ubyte", "u2": "uint16"}  # TODO implement al
 
 
 class Converter(object):
-    def __init__(self, input_js):
+
+    # TODO implement size lookup funtion
+
+    def __init__(self, input_js, is_master=False):
         print(self.__class__.__name__)
-        # self.input = {}
+        self.output = []
         self.subtrees = {}
         self.this_level_keys = []
         self.input = input_js
@@ -17,14 +20,36 @@ class Converter(object):
             print(self.this_level_keys)
         except:
             self.this_level_keys = None
+        if is_master:
+            self.global_init()
+            self.parse_subtree()
 
     def parse_subtree(self):
         for this_level_key in self.this_level_keys:
-            local_key = this_level_key
-            if str(this_level_key) == "doc-ref": local_key = 'doc'
-            self.__dict__[local_key] = self.input[this_level_key]
-            self.subtrees[local_key] = globals()[local_key](self.input[this_level_key])
-            self.subtrees[local_key].parse_subtree()
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                self.__dict__[local_key] = self.input[this_level_key]
+                self.subtrees[local_key] = globals()[local_key](self.input[this_level_key])
+                self.subtrees[local_key].parse_subtree()
+
+    def generate_code_toplevel(self):
+        self.output.extend(self.subtrees["enums"].generate_code())
+        self.output.extend(self.subtrees["types"].generate_code())
+        print("AAAAAAAAAAAAAAAAAAAAAAA")
+        self.output.extend(self.subtrees["seq"].generate_code())
+        print("AAAAAAAAAAAAAAAAAAAAAAA")
+
+        return self.output
+
+    def generate_code(self):
+        for this_level_key in self.this_level_keys:
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                try:
+                    self.output.extend(self.subtrees[local_key].generate_code())
+                except:
+                    pass
+        return self.output
 
     def print_input(self):
         print("+++++++++++++")
@@ -44,17 +69,14 @@ class Converter(object):
             self.subtrees[this_level_key].print_recursive_input()
             print("=============")
 
+    def global_init(self):
+        self.resolve_enum_sizes()
 
-class attribute():
-    def __init__(self, input_js, name=None):
-        self.input = input_js
-        if name is None:
-            self.id = self.input["id"]
-        else:
-            self.id = name
+    def resolve_enum_sizes(self):
+        self.enum_size = {"standard": "<byte>"}
 
-    def parse(self):
-        print(self.input)
+    def lookup_enum_size(self, enum):
+        return self.enum_size[enum]
 
 
 class meta(Converter):
@@ -71,7 +93,9 @@ class doc(Converter):
 
     def parse_subtree(self):
         self.output = "//     " + str(self.input).replace("\n", "\n//    ")
-        print(self.output)
+
+    def generate_code(self):
+        return [self.output]
 
 
 class enums(Converter):
@@ -79,7 +103,82 @@ class enums(Converter):
         Converter.__init__(self, input_js)
 
     def parse_subtree(self):
-        print(self.input)
+        for this_level_key in self.this_level_keys:
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                self.subtrees[local_key] = attribute(local_key, self.input[this_level_key])
+                print(str(self.subtrees[local_key].get_name()) + " : " + str(self.subtrees[local_key].get_value()))
+
+    def generate_code(self):
+        self.output = []
+        for enum in self.subtrees.keys():
+            self.output.extend(self.gen_single_enum(enum, self.subtrees[enum], converter.lookup_enum_size("standard")))
+            self.output.append("\n")
+        return self.output
+
+    def gen_single_enum(self, key, enumerations, size="<byte>"):
+        values = enumerations.get_value()
+        lines = []
+        # TODO FIND CORRECT TYPE? defaulting to <byte> for now
+        lines.append("enum " + size + " {")
+        keys = list(values.keys())
+        for k in keys[0:-1]:
+            lines.append("  " + (values[k] if "id" not in values[k] else values[k]["id"]) + " = " + str(k) + "," + (
+                "" if "doc" not in values[k] else "  // " + (values[k]["doc"]).replace("\n", "\n     //")))
+        lines.append(
+            "  " + (values[keys[-1]] if "id" not in values[keys[-1]] else values[keys[-1]]["id"]) + " = " + str(
+                keys[-1]) + "" + (
+                "" if "doc" not in values[keys[-1]] else "  // " + (values[keys[-1]]["doc"]).replace("\n",
+                                                                                                     "\n     //")))
+        lines.append("} " + str(key) + ";")
+        return lines
+
+
+class attribute():
+    def __init__(self, name, value):
+        self.value = value
+        self.name = name
+
+    def get_value(self):
+        return self.value
+
+    def get_name(self):
+        return self.name
+
+
+class data_point():
+    def __init__(self, input_js, name=None):
+        self.subtrees = dict()
+        self.input = input_js
+        self.id = name
+
+    def parse(self):
+        for this_level_key in self.input.keys():
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                self.subtrees[local_key] = attribute(local_key, self.input[this_level_key])
+
+            print(str(self.subtrees[local_key].get_name()) + " : " + str(self.subtrees[local_key].get_value()))
+        if self.id is None:
+            self.id = self.subtrees["id"].get_value()
+
+    def generate_code(self):
+        self.output = []
+        print(self.subtrees.keys())
+        if (len(self.subtrees.keys()) == 2) and ("type" in self.subtrees.keys()):
+            self.output.append(self.gen_atomic_variable(self.id, self.subtrees["type"].get_value()))
+            print(self.output)
+        return self.output
+
+    def gen_atomic_variable(self, name, type, size=None):
+        output = ""
+        print(name)
+        if size is not None:
+            print("AAA")
+            pass
+        if str(type) in datatypes.keys():
+            return "    " + str(datatypes[type]) + " " + str(name) + ";"
+        return "    " + str(type) + " " + str(name) + ";"
 
 
 class seq(Converter):
@@ -87,10 +186,17 @@ class seq(Converter):
         Converter.__init__(self, input_js)
 
     def parse_subtree(self):
-        for attribute_dict in self.input:
+        self.this_level_keys = []
+        for data_dict in self.input:
             # if this_level_key == "doc-ref":this_level_key="doc"
-            self.subtrees[attribute_dict["id"]] = attribute(attribute_dict)
-            self.subtrees[attribute_dict["id"]].parse()
+            self.subtrees[data_dict["id"]] = data_point(data_dict)
+            self.subtrees[data_dict["id"]].parse()
+            self.this_level_keys.append(data_dict["id"])
+
+    def generate_code(self):
+        for this_level_key in self.this_level_keys:
+            self.output.extend(self.subtrees[this_level_key].generate_code())
+        return self.output
 
 
 class instances(seq):
@@ -100,11 +206,14 @@ class instances(seq):
 
     def parse_subtree(self):
         for this_level_key in self.this_level_keys:
-            local_key = this_level_key
-            if this_level_key == "doc-ref": local_key = "doc"
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                self.subtrees[local_key] = data_point(self.input[local_key], local_key)
+                self.subtrees[local_key].parse()
 
-            self.subtrees[local_key] = attribute(self.input[local_key], local_key)
-            self.subtrees[local_key].parse()
+    def generate_code(self):
+        # TODO understand what these are exactly supposed to do as the are not parsed
+        pass
 
 
 class types(Converter):
@@ -113,15 +222,42 @@ class types(Converter):
 
     def parse_subtree(self):
         print(self.input)
-
         for this_level_key in self.this_level_keys:
-            local_key = this_level_key
-            if str(this_level_key) == "doc-ref": local_key = 'doc'
-            self.subtrees[local_key] = Converter(self.input[this_level_key])
-            self.subtrees[local_key].parse_subtree()
+            local_key = remap_keys(this_level_key)
+            if local_key is not None:
+                self.subtrees[local_key] = Converter(self.input[this_level_key])
+                self.subtrees[local_key].parse_subtree()
+
+    def generate_code(self):
+        self.output.extend(self.gen_forward_types())
+        self.output.extend(self.gen_complete_types())
+        return self.output
+
+    def gen_complete_types(self):
+        output = []
+        for this_level_key in self.this_level_keys:
+            output.extend(self.subtrees[this_level_key].generate_code())
+        return output
+        # TODO IMPLEMENT THIS IS JUST A PLACEHOLDER
+
+    def gen_forward_types(self):
+        output = []
+        for this_level_key in self.this_level_keys:
+            output.append("struct " + str(this_level_key) + ";")
+        return output
+
+
+def remap_keys(key):
+    remap = {"doc-ref": "doc"}
+    blocklist = ["-webide-representation"]
+    if key in blocklist: return None
+    if key in remap.keys(): return remap[key]
+    return key
 
 
 def main():
+    global converter
+
     if len(sys.argv) != 3:
         print("USAGE = python3 Converter.py <input file path> <output file path>")  # TODO
         exit(1)
@@ -131,45 +267,18 @@ def main():
         input_file = in_file.read()
     kaitaijs = yaml.load(input_file)
 
-    converter = Converter(kaitaijs)
-    converter.parse_subtree()
+    converter = Converter(kaitaijs, True)
+
+    # output = converter.generate_code_toplevel()
+    output = converter.generate_code()
+    print('\n'.join(output))
     # converter.print_input()
     exit(0)
 
-    types = kaitaijs["types"]
-    enums = kaitaijs["enums"]
-    print(kaitaijs)
-    lines = []
-    lines.extend(gen_enums(enums))
-    lines.extend(gen_types(types))
     with open(output_file_name, "w") as out_file:
-        out_file.write('\n'.join(lines))
+        out_file.write('\n'.join(output))
 
-    print('\n'.join(lines))
-
-
-def gen_enums(enums):
-    lines = []
-    for enum_key in enums.keys():
-        lines.extend(gen_single_enum(enum_key, enums[enum_key]))
-        lines.append("\n")
-    return lines
-
-
-def gen_single_enum(key, values):
-    lines = []
-    # TODO FIND CORRECT TYPE? defaulting to <byte> for now
-    size = "<byte>"
-    lines.append("enum " + size + " {")
-    keys = list(values.keys())
-    for k in keys[0:-1]:
-        lines.append("  " + (values[k] if "id" not in values[k] else values[k]["id"]) + " = " + str(k) + "," + (
-            "" if "doc" not in values[k] else "  // " + (values[k]["doc"]).replace("\n", "\n     //")))
-    lines.append("  " + (values[keys[-1]] if "id" not in values[keys[-1]] else values[keys[-1]]["id"]) + " = " + str(
-        keys[-1]) + "" + ("" if "doc" not in values[keys[-1]] else "  // " + (values[keys[-1]]["doc"]).replace("\n",
-                                                                                                               "\n     //")))
-    lines.append("} " + str(key) + ";")
-    return lines
+    print('\n'.join(output))
 
 
 def gen_types(types):
@@ -217,11 +326,6 @@ def gen_simple_var_creation(typ, name, content=None):  # TODO Refactor name
     else:
         print("COMPLEX VARS not YET supported!\n")  # TODO implement
     lines += ";"
-    return lines
-
-
-def gen_switch():
-    lines = []
     return lines
 
 
