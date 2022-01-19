1016c08c6f95cffedcf79f41df79af171657b1ba
GamerPanda99 <hno3@pm.me>
2022-01-19 16:21:39 +0100

    [testbench] fixed a little error

---
+++ Converter/main.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+import test.testbench as tb
+import argparse
+import logging as log
+import subprocess as sub
+
+#TODO add orchestration for batch run here.
+# TODO skip auto file gen if BT doesn't support it
+# TODO create convert /(& build) only mode+
+
+
+def run_tests_on_all():
+    pass
+
+
+def convert_all():
+    pass
+
+
+def main():
+    pass
